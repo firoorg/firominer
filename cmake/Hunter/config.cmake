cfbbf177a7c3941d09e329f3d805fa773f68c999
WTRMQDev <TetsuwanAtomu@tuta.io>
2019-07-24 17:38:26 +0000

    Update to 0.9.3

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,7 +1,7 @@
 hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_USE_OPENSSL=OFF CMAKE_USE_LIBSSH2=OFF CURL_CA_PATH=none)
 hunter_config(Boost VERSION 1.66.0)
 
-hunter_config(ethash VERSION 0.4.1
-    URL https://github.com/chfast/ethash/archive/v0.4.1.tar.gz
-    SHA1 12a7ad52809ca74f2a5e8f849408f0ebf795719c
+hunter_config(ethash VERSION 0.5.1
+    URL https://github.com/leerproject/cpp-ethash/archive/0.5.1-alpha.1.1.tar.gz
+    SHA1 1f02418e00ba65b8d191aee93cb7aaccb54a29fd
 )
