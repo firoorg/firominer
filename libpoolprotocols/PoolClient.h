f2fbdc6e59a20311c83308efeb16e02f0af60d59
Reuben Yap <reuben@firo.org>
2026-09-14 23:25:01 +0800

    Reduce build deprecation warnings while preserving GPU support

--- libpoolprotocols/PoolClient.h
+++ libpoolprotocols/PoolClient.h
@@ -3,7 +3,7 @@
 #include <queue>
 
 #include <boost/asio/ip/address.hpp>
-#include <boost/bind.hpp>
+#include <boost/bind/bind.hpp>
 
 #include <libethcore/Miner.h>
 #include <libpoolprotocols/PoolURI.h>
